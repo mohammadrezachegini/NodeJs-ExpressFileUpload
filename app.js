3a018a2009e3621d037e5943d963f1519b8ebe6e
Goodarzvand Chegini <goodarzvandcheginim@student.douglascollege.ca>
2023-03-04 13:23:25 -0800

    create upload file method

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express")
+const fileUpload = require("express-fileupload")
 const {ErrorHandler,NotFoundError} = require("./util/errorHandler")
+const fs = require("fs")
+const path = require("path")
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
+// app.use(fileUpload({abortOnLimit: true, limits: {
+//     fileSize: 150000,
+//     fields:3
+// }}))
 
+app.use(fileUpload())
 
 app.post("/upload", (req,res) => {
+    const image = req.files.image
+    const ext = path.extname(image.name)
+    const destPath = path.join(__dirname, "public","upload", Date.now() + ext)
+    const buffer = Buffer.from(image.data)
+    fs.writeFileSync(destPath, buffer)
+    res.send(req.files)
+    
 
 })
 
